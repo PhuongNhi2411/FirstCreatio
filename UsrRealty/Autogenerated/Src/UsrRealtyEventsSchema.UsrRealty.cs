a93477468be418302ce8fc64cb131e0836e61284
phuongnhi <phuongnhi2411@gmail.com>
2024-09-01 23:41:36 +0700

    Realty section updated

---
+++ UsrRealty/Autogenerated/Src/UsrRealtyEventsSchema.UsrRealty.cs
@@ -0,0 +1,57 @@
+﻿namespace Terrasoft.Configuration
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Globalization;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+
+	#region Class: UsrRealtyEventsSchema
+
+	/// <exclude/>
+	public class UsrRealtyEventsSchema : Terrasoft.Core.SourceCodeSchema
+	{
+
+		#region Constructors: Public
+
+		public UsrRealtyEventsSchema(SourceCodeSchemaManager sourceCodeSchemaManager)
+			: base(sourceCodeSchemaManager) {
+		}
+
+		public UsrRealtyEventsSchema(UsrRealtyEventsSchema source)
+			: base( source) {
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void InitializeProperties() {
+			base.InitializeProperties();
+			UId = new Guid("30c3018f-0cb4-428a-bfcc-fc8ae6c8dc32");
+			Name = "UsrRealtyEvents";
+			ParentSchemaUId = new Guid("50e3acc0-26fc-4237-a095-849a1d534bd3");
+			CreatedInPackageId = new Guid("93548035-7a5e-43dc-8022-6e17429c07a7");
+			ZipBody = new byte[] { 31,139,8,0,0,0,0,0,4,0,83,208,211,211,3,0,43,31,116,100,4,0,0,0 };
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public override void GetParentRealUIds(Collection<Guid> realUIds) {
+			base.GetParentRealUIds(realUIds);
+			realUIds.Add(new Guid("30c3018f-0cb4-428a-bfcc-fc8ae6c8dc32"));
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
